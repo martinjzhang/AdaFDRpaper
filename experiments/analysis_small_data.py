cb7bc2d22652f8041747795e1aa31b98824cb0a7
martinjzhang <martinjzhang@gmail.com>
2018-10-30 12:35:36 -0700

    migrate to adafdr package

--- experiments/analysis_small_data.py
+++ experiments/analysis_small_data.py
@@ -5,8 +5,8 @@ import logging
 import os
 import sys
 import argparse
-import nfdr2.data_loader as dl
-import nfdr2.method as md
+import adafdr.data_loader as dl
+import adafdr.method as md
 import time
 import matplotlib.pyplot as plt
 import pickle
@@ -39,12 +39,12 @@ def main(args):
     logger.info('# p: %s'%str(p[0:2]))
     logger.info('# x: %s'%str(x[0:2, :]))
     # Report the baseline methods.
-    n_rej, t_rej = md.bh(p, alpha=alpha, n_full=n_full, verbose=False)
+    n_rej, t_rej = md.bh_test(p, alpha=alpha, n_full=n_full, verbose=False)
     logger.info('## BH, n_rej=%d, t_rej=%0.5f'%(n_rej,t_rej))
-    n_rej, t_rej, pi0_hat = md.storey_bh(p, alpha=alpha, n_full=n_full, verbose=False)
+    n_rej, t_rej, pi0_hat = md.sbh_test(p, alpha=alpha, n_full=n_full, verbose=False)
     logger.info('## SBH, n_rej=%d, t_rej=%0.5f, pi0_hat=%0.3f'%(n_rej, t_rej, pi0_hat))    
     # Analysis
-    md.feature_explore(p, x, alpha=alpha, n_full=n_full, vis_dim=None, cate_name=cate_name,\
+    md.adafdr_explore(p, x, alpha=alpha, n_full=n_full, vis_dim=None, cate_name=cate_name,\
                        output_folder=output_folder, h=None)
     # Fast mode.
     output_folder_fast = output_folder + '_fast'
@@ -56,18 +56,18 @@ def main(args):
     logger.info('# p: %s'%str(p[0:2]))
     logger.info('# x: %s'%str(x[0:2, :]))
     start_time = time.time()
-    n_rej,t_rej,_= md.method_hs(p, x, K=5, alpha=alpha, h=None, n_full=n_full, n_itr=n_itr,\
-                                verbose=True, output_folder=output_folder_fast, random_state=0,\
-                                fast_mode=True)
-    logger.info('## nfdr2 (fast mode), n_rej1=%d, n_rej2=%d, n_rej_total=%d'%(n_rej[0],n_rej[1],n_rej[0]+n_rej[1]))
+    n_rej,t_rej,_= md.adafdr_test(p, x, K=5, alpha=alpha, h=None, n_full=n_full, n_itr=n_itr,\
+                                  verbose=True, output_folder=output_folder_fast, random_state=0,\
+                                  fast_mode=True)
+    logger.info('## AdaFDR (fast mode), n_rej1=%d, n_rej2=%d, n_rej_total=%d'%(n_rej[0],n_rej[1],n_rej[0]+n_rej[1]))
     logger.info('## Total time (fast mode): %0.1fs'%(time.time()-start_time))
     # Full mode.
     logger.info('# p: %s'%str(p[0:2]))
     logger.info('# x: %s'%str(x[0:2, :]))
     start_time = time.time()
-    n_rej,t_rej,_= md.method_hs(p, x, K=5, alpha=alpha, h=None, n_full=n_full, n_itr=n_itr,\
-                                verbose=True, output_folder=output_folder, random_state=0)
-    logger.info('## nfdr2, n_rej1=%d, n_rej2=%d, n_rej_total=%d'%(n_rej[0],n_rej[1],n_rej[0]+n_rej[1]))
+    n_rej,t_rej,_= md.adafdr_test(p, x, K=5, alpha=alpha, h=None, n_full=n_full, n_itr=n_itr,\
+                                  verbose=True, output_folder=output_folder, random_state=0)
+    logger.info('## AdaFDR, n_rej1=%d, n_rej2=%d, n_rej_total=%d'%(n_rej[0],n_rej[1],n_rej[0]+n_rej[1]))
     logger.info('## Total time: %0.1fs'%(time.time()-start_time))
     
 if __name__ == '__main__':
